a18679cb1ca1a771823e02e57561c260ae570756
Gabriel-SDO <60404154+Gabriel-SDO@users.noreply.github.com>
2021-04-26 16:43:36 -0300

    ajustes iniciais

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB2OlAKrquYvrsa7qHbxjMsE6Xpo13zUNM",
+    authDomain: "primeiro-projeto-cdb0f.firebaseapp.com",
+    projectId: "primeiro-projeto-cdb0f",
+    storageBucket: "primeiro-projeto-cdb0f.appspot.com",
+    messagingSenderId: "158086693105",
+    appId: "1:158086693105:web:9073e7b9a9bc2e9bcb2950"
+  }
 };
