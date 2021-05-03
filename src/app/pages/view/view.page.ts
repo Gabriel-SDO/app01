b5ed978b701e2165e806c5da1d5ccc2f979fe0cd
Gabriel-SDO <60404154+Gabriel-SDO@users.noreply.github.com>
2021-05-03 14:11:35 -0300

    atualizado

--- src/app/pages/view/view.page.ts
+++ src/app/pages/view/view.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// 1) Importa dependências
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-view',
   templateUrl: './view.page.html',
@@ -7,7 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewPage implements OnInit {
 
-  constructor() { }
+  // 3) Atributos
+  private itemDoc: AngularFirestoreDocument<any>;
+  public item: Observable<any>;
+  private id: string;
+
+  constructor(
+
+    // 2) Injeta dependências
+    private afs: AngularFirestore,
+    private route: ActivatedRoute
+  ) {
+
+    // Obtér o ID do artigo a ser exibido
+    this.id = this.route.snapshot.paramMap.get('id');
+
+    // Consulta o banco de dados
+    this.itemDoc = afs.doc<any>(`articles/${this.id}`);
+    this.item = this.itemDoc.valueChanges();
+   }
 
   ngOnInit() {
   }
